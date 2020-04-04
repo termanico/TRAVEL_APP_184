e02918be60ea8036ca2749a0bc98c2287b20d9e7
termanico <termanico@users.noreply.github.com>
2020-04-04 13:14:56 +0200

    Add files via upload
    Initial Test Upload from Orion

---
+++ TRAVEL_APP_184/dist/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("namespace_184travel_app_184.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("namespace_184travel_app_184.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
